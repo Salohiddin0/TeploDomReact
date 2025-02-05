5d558069c61cedf64632e08f55434b2e4419b4c5
Your GitHub Username <Your GitHub Email>
2025-02-06 00:01:03 +0500

    TeploDomReact

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <div className='bg-[#fafafa]'>
+      <Nav />
+      <Header />
+    </div>
+  )
+}
+
+export default Home
